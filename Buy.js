c90141a0e1d058f3e6b9666e25a52627be3e2622
alielsadat1 <pwbcgzn7yb@privaterelay.appleid.com>
2025-10-31 21:03:29 +0300

    Add files via upload

---
+++ Buy.js
@@ -0,0 +1,9 @@
+/*عند الضغط على زر المينيو يظهر*/
+function openMenu() {
+    document.getElementById("sidebar").style.left = "0";
+}
+
+function closeMenu() {
+    document.getElementById("sidebar").style.left = "-250px";
+}
+
